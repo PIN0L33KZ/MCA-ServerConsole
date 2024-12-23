0e8f27c877398a17b9590312a9259fe7bb0498fc
PIN0L33KZ <76883164+PIN0L33KZ@users.noreply.github.com>
2024-12-23 02:03:46 +0100

    FIXED: set scaling to dpi mode instead of font

--- MCA-ServerConsole/Forms/FRM_Console.Designer.cs
+++ MCA-ServerConsole/Forms/FRM_Console.Designer.cs
@@ -294,8 +294,8 @@
             // 
             // FRM_Console
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             ClientSize = new Size(1010, 501);
             Controls.Add(PNL_Fill);
             Controls.Add(PNL_Left);
